7269d4aef599b06af6003d88472e1db51dc06fa9
Rahul Gupta <rahulgupta@itg-mbp-5084.home>
2024-02-16 15:54:09 +0000

    automation framewrok structure

---
+++ src/test/java/stepDefs/HamBurger.java
@@ -0,0 +1,17 @@
+package stepDefs;
+import io.cucumber.java.en.Given;
+
+public class HamBurger {
+
+    @Given("user is on the Serravalle Website")
+    public void userIsOnTheSerravalleWebsite() {
+    }
+
+    @Given("user click on the ham burger menu")
+    public void userClickOnHamBurgerMenu() {
+    }
+
+    @Given("address detail are displayed")
+    public void verifyAddressDetails() {
+    }
+}
---
+++ src/test/resources/features/HamBurger.feature
@@ -0,0 +1,6 @@
+Feature: Home Page-Ham Burger Menu
+
+Scenario: Verify address details
+Given user is on the Serravalle Website
+When user click on the ham burger menu
+Then address detail are displayed
